f4a82ab16c666f77a7bd6406ab8eb0a72d1bbfd7
Aaron Boodman <aaron@aaronboodman.com>
2022-01-22 00:08:04 -1000

    Update reps: Add logLevel param to Server constructor to make log level configurable.

--- main.ts
+++ main.ts
@@ -33,7 +33,7 @@ app.prepare().then(() => {
   );
   const webSocketServer = new WebSocket.Server({ noServer: true });
 
-  const reps = new Server(mutators);
+  const reps = new Server(mutators, "debug");
 
   httpServer.on("upgrade", (req, socket, head) => {
     const { pathname } = parse(req.url!, true);
