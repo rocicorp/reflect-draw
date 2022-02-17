8a64086994310db2bbdce09c2a7d57c227112047
Erik Arvidsson <erik.arvidsson@gmail.com>
2022-02-17 04:30:15 +0100

    feat: Hook up datadog logger (#25)
    * feat: Hook up datadog logger
    
    * Few fixes to get DatadogLogger working.
    
    Co-authored-by: Aaron Boodman <aaron@aaronboodman.com>

--- worker/index.ts
+++ worker/index.ts
@@ -1,9 +1,18 @@
-import { Server as BaseServer } from "reps-do";
+import { DatadogLogger, Server as BaseServer } from "reps-do";
 export { worker as default } from "reps-do";
 import { mutators, type M } from "../src/datamodel/mutators.js";
 
 export class Server extends BaseServer<M> {
-  constructor(state: DurableObjectState) {
-    super(mutators, state);
+  constructor(state: DurableObjectState, env: Record<string, string>) {
+    const logger = new DatadogLogger({
+      apiKey: env.DATADOG_API_KEY,
+      service: "replidraw",
+    });
+
+    super({
+      mutators,
+      state,
+      logger,
+    });
   }
 }
