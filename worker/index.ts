bfbc0f68e119c620c2bbba01161d4e6bd79d561c
Aaron Boodman <aaron@aaronboodman.com>
2022-02-19 18:28:49 -0800

    Fix datadog logging

--- worker/index.ts
+++ worker/index.ts
@@ -10,7 +10,7 @@ import { mutators, type M } from "../src/datamodel/mutators.js";
 export class Server extends BaseServer<M> {
   constructor(state: DurableObjectState, env: Record<string, string>) {
     let logger = consoleLogger;
-    if (env.DATADOG_LOG_LEVEL) {
+    if (env.DATADOG_API_KEY) {
       logger = new TeeLogger([
         logger,
         new DatadogLogger({
