13c447f4cfe4ede906051ca16d15f47f03098efb
Greg Baker <greg@roci.dev>
2022-02-21 17:07:53 -0700

    feat: Update to reps-do 3.0.0 and setup worker logging (#30)

--- worker/index.ts
+++ worker/index.ts
@@ -1,48 +1,64 @@
 import {
   consoleLogger,
   DatadogLogger,
+  Logger,
   Server as BaseServer,
   TeeLogger,
 } from "reps-do";
-import { createWorker } from "reps-do";
+import { createWorker, BaseWorkerEnv } from "reps-do";
 import { mutators, type M } from "../src/datamodel/mutators.js";
+
+function createLogger(service: string, datadogApiKey?: string): Logger {
+  let logger = consoleLogger;
+  if (datadogApiKey) {
+    logger = new TeeLogger([
+      logger,
+      new DatadogLogger({
+        apiKey: datadogApiKey,
+        service,
+      }),
+    ]);
+  }
+  return logger;
+}
 
 export class Server extends BaseServer<M> {
   constructor(state: DurableObjectState, env: Record<string, string>) {
-    let logger = consoleLogger;
-    if (env.DATADOG_API_KEY) {
-      logger = new TeeLogger([
-        logger,
-        new DatadogLogger({
-          apiKey: env.DATADOG_API_KEY,
-          service: "replidraw",
-        }),
-      ]);
-    }
-
     super({
       mutators,
       state,
-      logger,
+      logger: createLogger("replidraw-do", env.DATADOG_API_KEY),
       logLevel: "info",
     });
   }
 }
 
-export default createWorker(async (auth: string, roomID: string) => {
-  // Note a real implementation should use signed and encrypted auth tokens,
-  // or store the auth tokens in a session database for validation.
-  const authJson = JSON.parse(auth);
-  if (!authJson) {
-    throw Error("Empty auth");
-  }
-  if (authJson.roomID !== roomID) {
-    throw new Error("incorrect roomID");
-  }
-  if (!authJson.userID || typeof authJson.userID !== "string") {
-    throw new Error("Missing userID");
-  }
-  return {
-    userID: authJson.userID,
-  };
+interface WorkerEnv extends BaseWorkerEnv {
+  DATADOG_API_KEY?: string;
+}
+
+export default createWorker<WorkerEnv>({
+  authHandler: async (auth: string, roomID: string) => {
+    // Note a real implementation should use signed and encrypted auth tokens,
+    // or store the auth tokens in a session database for validation.
+    const authJson = JSON.parse(auth);
+    if (!authJson) {
+      throw Error("Empty auth");
+    }
+    if (authJson.roomID !== roomID) {
+      throw new Error("incorrect roomID");
+    }
+    if (!authJson.userID || typeof authJson.userID !== "string") {
+      throw new Error("Missing userID");
+    }
+    return {
+      userID: authJson.userID,
+    };
+  },
+  createLogger(env: WorkerEnv) {
+    return createLogger("replidraw-worker", env.DATADOG_API_KEY);
+  },
+  getLogLevel(_env: WorkerEnv) {
+    return "debug";
+  },
 });
