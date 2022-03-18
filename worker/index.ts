5ecdb71165d81a4247d9b1e9b281c79f8ffb0bde
Gregory Baker <greg@roci.dev>
2022-03-18 16:11:53 -0700

    feature: Update to reflect@0.5.0, use new createReflect api, and setup REFLECT_AUTH_API_KEY secret

--- worker/index.ts
+++ worker/index.ts
@@ -2,63 +2,52 @@ import {
   consoleLogger,
   DatadogLogger,
   Logger,
-  Server as BaseServer,
   TeeLogger,
+  createReflect,
+  ReflectBaseEnv,
 } from "reflect";
-import { createWorker, BaseWorkerEnv } from "reflect";
 import { mutators, type M } from "../src/datamodel/mutators.js";
 
-function createLogger(service: string, datadogApiKey?: string): Logger {
+function createLogger(env: ReplidrawEnv): Logger {
   let logger = consoleLogger;
-  if (datadogApiKey) {
+  console.log(env.REFLECT_AUTH_API_KEY);
+  if (env.DATADOG_API_KEY) {
     logger = new TeeLogger([
       logger,
       new DatadogLogger({
-        apiKey: datadogApiKey,
-        service,
+        apiKey: env.DATADOG_API_KEY,
       }),
     ]);
   }
   return logger;
 }
 
-export class Server extends BaseServer<M> {
-  constructor(state: DurableObjectState, env: Record<string, string>) {
-    super({
-      mutators,
-      state,
-      logger: createLogger("replidraw-do", env.DATADOG_API_KEY),
-      logLevel: "info",
-    });
-  }
-}
-
-interface WorkerEnv extends BaseWorkerEnv {
+interface ReplidrawEnv extends ReflectBaseEnv {
   DATADOG_API_KEY?: string;
 }
 
-export default createWorker<WorkerEnv>({
-  authHandler: async (auth: string, roomID: string) => {
-    // Note a real implementation should use signed and encrypted auth tokens,
-    // or store the auth tokens in a session database for validation.
-    const authJson = JSON.parse(auth);
-    if (!authJson) {
-      throw Error("Empty auth");
-    }
-    if (authJson.roomID !== roomID) {
-      throw new Error("incorrect roomID");
-    }
-    if (!authJson.userID || typeof authJson.userID !== "string") {
-      throw new Error("Missing userID");
-    }
-    return {
-      userID: authJson.userID,
-    };
-  },
-  createLogger(env: WorkerEnv) {
-    return createLogger("replidraw-worker", env.DATADOG_API_KEY);
-  },
-  getLogLevel(_env: WorkerEnv) {
-    return "debug";
-  },
+const authHandler = async (auth: string, roomID: string) => {
+  // Note a real implementation should use signed and encrypted auth tokens,
+  // or store the auth tokens in a session database for validation.
+  const authJson = JSON.parse(auth);
+  if (!authJson) {
+    throw Error("Empty auth");
+  }
+  if (authJson.roomID !== roomID) {
+    throw new Error("incorrect roomID");
+  }
+  if (!authJson.userID || typeof authJson.userID !== "string") {
+    throw new Error("Missing userID");
+  }
+  return {
+    userID: authJson.userID,
+  };
+};
+
+const { worker, RoomDO, AuthDO } = createReflect({
+  mutators,
+  authHandler,
+  createLogger,
+  getLogLevel: () => "debug",
 });
+export { worker as default, RoomDO, AuthDO };
