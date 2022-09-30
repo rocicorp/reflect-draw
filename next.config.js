9ee3fdee4c9e726c738d68b849be5f2b17c6419b
Gregory Baker <greg@roci.dev>
2022-09-29 19:42:33 -0700

    no minimize

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+module.exports = (nextConfig) => {
+  return {
+    ...nextConfig,
+    webpack(webpackConfig) {
+      return {
+        ...webpackConfig,
+        optimization: {
+          minimize: false,
+        },
+      };
+    },
+  };
 };
