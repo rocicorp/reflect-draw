0f78759c1c5d10e04136df4452b5eee4bfa9f641
Aaron Boodman <aaron@aaronboodman.com>
2022-12-17 01:09:55 -1000

    Simplify error handling:
    - we don't need a special failed page, nextjs does this for us
    - we don't need to retry room creation, the nanoid has enough
      entropy that collision likelihood is vanishing
    
    The purpose of these is teaching and we just want to focus on the
    specific thing we are teaching. Putting normal good-practice
    software dev stuff in there muddies the lesson.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { nanoid } from "nanoid";
-import { env } from "process";
 import { consoleLogSink, OptionalLoggerImpl } from "@rocicorp/logger";
 import { DataDogBrowserLogSink } from "../frontend/data-dog-browser-log-sink";
 import { workerURL } from "../util/host";
@@ -13,28 +12,12 @@ function Page() {
   return "";
 }
 
-// We select a roomID probabilistically. If it is taken or there is some
-// other error when we try to create it, try a total of this many times
-// before giving up.
-const maxCreateRoomTries = 2;
-
 export async function getServerSideProps() {
-  for (let i = 0; i < maxCreateRoomTries; i++) {
-    const newRoomID = nanoid(6);
-    const created = await maybeCreateRoom(newRoomID);
-    if (created) {
-      return {
-        redirect: {
-          destination: `/d/${newRoomID}`,
-          permanent: false,
-        },
-      };
-    }
-  }
-
+  const newRoomID = nanoid(6);
+  await createRoom(newRoomID);
   return {
     redirect: {
-      destination: `/d/failed`,
+      destination: `/d/${newRoomID}`,
       permanent: false,
     },
   };
@@ -46,23 +29,21 @@ const createRoomURL = new URL("/createRoom", workerURL).toString();
 
 const createRoomHeaders = new Headers();
 createRoomHeaders.set("Content-Type", "application/json");
-const reflectApiKey = env.REFLECT_API_KEY || "";
+const reflectApiKey = process.env.REFLECT_API_KEY || "";
 createRoomHeaders.set("x-reflect-auth-api-key", reflectApiKey);
 
-async function maybeCreateRoom(roomID: string) {
+async function createRoom(roomID: string) {
   logger.info?.(`Creating room '${roomID}' at ${createRoomURL}`);
   const createRoomResponse = await fetch(createRoomURL, {
     method: "POST",
     headers: createRoomHeaders,
     body: JSON.stringify({ roomID }),
   });
-  if (createRoomResponse.status === 200) {
-    return true;
+  if (createRoomResponse.status !== 200) {
+    throw new Error(
+      `Failed to create room ${roomID}: ${
+        createRoomResponse.status
+      }: ${await createRoomResponse.text()}`
+    );
   }
-  logger.error?.(
-    `Failed to create room ${roomID}: ${
-      createRoomResponse.status
-    }: ${await createRoomResponse.text()}`
-  );
-  return false;
 }
