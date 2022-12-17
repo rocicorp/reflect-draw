09f941aa6dce0d7b6a1397dbda3484c01d1c6229
Aaron Boodman <aaron@aaronboodman.com>
2022-12-17 01:01:35 -1000

    Environment variables don't get put in the browser bundle when imported.
    Sigh.

--- src/util/host.ts
+++ src/util/host.ts
@@ -1,8 +1,10 @@
-import { env } from "process";
-
 // The websocket address will be ws:// when developing locally or wss://
 // in production. Likewise the reflect server is accessible via http://
 // in development and https:// in production.
+//
+// Note: process.env has to be used instead of importing env because Next.js
+// apparently does string substitution.
 export const workerWsURI =
-  env.NEXT_PUBLIC_WORKER_HOST ?? "wss://replidraw.replicache.workers.dev";
+  process.env.NEXT_PUBLIC_WORKER_HOST ??
+  "wss://replidraw.replicache.workers.dev";
 export const workerURL = workerWsURI.replace(/^ws/, "http");
