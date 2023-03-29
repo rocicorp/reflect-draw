89f4ccd9659669b8822b030fb9b25c0aaf4c0039
Gregory Baker <greg@roci.dev>
2023-03-28 18:43:29 -0700

    remove metric arg

--- src/pages/d/[id].tsx
+++ src/pages/d/[id].tsx
@@ -46,7 +46,6 @@ export default function Home() {
         logSinks,
         logLevel: "debug",
         mutators: clientMutators,
-        metrics,
       });
 
       const defaultUserInfo = randUserInfo();
