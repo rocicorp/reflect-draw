39e131c0a8107b9f75b9c9f7a8b8102eec9c9b45
Aaron Boodman <aaron@aaronboodman.com>
2024-01-31 16:35:22 -1000

    Turn NEXT_PUBLIC_REFLECT_SERVER into a template (😳).

--- src/util/host.ts
+++ src/util/host.ts
@@ -1,1 +1,12 @@
-export const reflectServer = process.env.NEXT_PUBLIC_REFLECT_SERVER ?? "";
+export const reflectServer = process.env.NEXT_PUBLIC_REFLECT_SERVER
+  ? applyTemplate(process.env.NEXT_PUBLIC_REFLECT_SERVER)
+  : "";
+
+function applyTemplate(template: string) {
+  const f = new Function(
+    "NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF",
+    `return \`${template}\``
+  );
+  const branchName = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ?? "";
+  return f(branchName.replace(/\//g, "-"));
+}
