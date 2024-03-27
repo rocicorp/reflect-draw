954d28188b7a017b52fdd160eb3fdc834bc1eacc
Cesar Alaestante <cesar@roci.dev>
2024-03-27 12:17:10 -0700

    start-load update

--- flood-test/start-load.sh
+++ flood-test/start-load.sh
@@ -6,9 +6,9 @@ PID_FILE="pids.txt"
 # Ensure the PID file is empty by overwriting it with an empty content
 : > "$PID_FILE"
 
-# Loop to execute the command 50 times in the background, disabling all output
+# Loop to execute the command 15 times in the background, disabling all output
 for i in {1..15}; do
-    npx element run load-test.ts >/dev/null 2>&1 &
+    npx element run monday-canvas-load.ts >/dev/null 2>&1 &
     echo $! >> "$PID_FILE"
 done
 
